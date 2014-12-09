bee7276fa04587a0ac73c8475e459cda8435e5f5
biramaster <andre_rubira@hotmail.com>
2014-12-09 10:34:46 +0100

    Börja convertera coordinator mappen

---
+++ EnviromentCrime/coordinator/incl/footer.php
@@ -0,0 +1,7 @@
+﻿<footer>
+					&copy; Småstads Kommun All rights reserved.
+				</footer>
+			</div><!-- End Content -->
+		</div><!-- End Wrapper -->
+	</body>
+</html>
---
+++ EnviromentCrime/coordinator/incl/header.php
@@ -0,0 +1,23 @@
+﻿<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="sv" lang="sv">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Småstads kommun</title>
+		<link rel="stylesheet" href="../stylesheet/style.css" />
+	</head>
+
+	<body>
+		<div id="wrapper">
+			<div id="logo">
+				<h1>Småstad</h1>
+				<p>Kommunen där vi bryr oss om varandra</p>
+			</div><!-- End Logo -->
+
+			<nav>
+				<ul>
+					<li><a class="reports" href="startCoordinator.html">Start(Ärenden)</a></li>
+					<li><a class="reportAdd" href="reportCrime.html">Rapportera brott</a></li>
+					<li class="right"><a class="logout" href="../index.html">Logga ut</a></li>
+				</ul>
+			</nav><!-- End Nav -->
