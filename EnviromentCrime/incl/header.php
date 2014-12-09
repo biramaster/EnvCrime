722536774d9d27a80201fda39b1e78958550541c
biramaster <biramaster001@gmail.com>
2014-12-09 09:01:59 +0100

    Convertera index.html till index.php

---
+++ EnviromentCrime/incl/header.php
@@ -0,0 +1,27 @@
+﻿<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="sv" lang="sv">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Småstads kommun</title>
+		<link rel="stylesheet" href="stylesheet/style.css" />
+	</head>
+
+	<body>
+		<div id="wrapper">
+			<div id="logo">
+				<h1>Småstad</h1>
+				<p>Kommunen där vi bryr oss om varandra</p>
+			</div><!-- End Logo -->
+                    
+			<nav>
+				<ul>
+                    
+
+					<li><a href="index.html">Hem</a></li>
+					<li><a href="citizen/about/services.html">Tjänster</a></li>
+					<li><a href="citizen/about/faq.html">FAQ</a></li>
+					<li><a href="citizen/about/contact.html">Kontakt</a></li>
+					<li class="right"><a class="login" href="login.html">Logga in (endast personal)</a></li>
+				</ul>
+			</nav><!-- End Nav -->
--- EnviromentCrime/index.php
+++ EnviromentCrime/index.php
@@ -1,32 +1,6 @@
-﻿<!DOCTYPE html>
-
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="sv" lang="sv">
-	<head>
-		<meta charset="UTF-8"/>
-		<title>Småstads kommun</title>
-		<link rel="stylesheet" href="stylesheet/style.css" />
-	</head>
-
-	<body>
-		<div id="wrapper">
-			<div id="logo">
-				<h1>Småstad</h1>
-				<p>Kommunen där vi bryr oss om varandra</p>
-			</div><!-- End Logo -->
-                  <!-- Anton was here -->  
-        <!--tja anton-->
-                <!-- hej -->  
-			<nav>
-				<ul>
-                    <!--hej på dig emil :)-->
-
-					<li><a href="index.html">Hem</a></li>
-					<li><a href="citizen/about/services.html">Tjänster</a></li>
-					<li><a href="citizen/about/faq.html">FAQ</a></li>
-					<li><a href="citizen/about/contact.html">Kontakt</a></li>
-					<li class="right"><a class="login" href="login.html">Logga in (endast personal)</a></li>
-				</ul>
-			</nav><!-- End Nav -->
+﻿<?php
+include("incl/header.php");  
+?>
 
 			<div id="content">
 				<section id="mainColumn">
@@ -80,10 +54,4 @@
 					<img src="images/pic_8.jpg" height="162" alt="Närbild av en fotboll" />
 				</aside><!-- End Right -->
 				
-				<footer>
-					&copy; Småstads Kommun All rights reserved.
-				</footer>
-			</div><!-- End Content -->
-		</div><!-- End Wrapper -->
-	</body>
-</html>
+				
