d353dd8255f686cbdd113eaffb98f40220c55aad
funkitown <Antonjthorn@gmail.com>
2014-12-09 09:11:30 +0100

    add header and footer to validate

--- EnviromentCrime/citizen/validate.php
+++ EnviromentCrime/citizen/validate.php
@@ -1,29 +1,5 @@
-<!DOCTYPE html>
-
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="sv" lang="sv">
-	<head>
-		<meta charset="UTF-8"/>
-		<title>Småstads kommun</title>
-		<link rel="stylesheet" href="../stylesheet/style.css" />
-	</head>
-
-	<body>
-		<div id="wrapper">
-			<div id="logo">
-				<h1>Småstad</h1>
-				<p>Kommunen där vi bryr oss om varandra</p>
-			</div><!-- End Logo -->
-
-				<nav>
-				<ul>
-					<li><a href="../index.html">Hem</a></li>
-					<li><a href="about/services.html">Tjänster</a></li>
-					<li><a href="about/faq.html">FAQ</a></li>
-					<li><a href="about/contact.html">Kontakt</a></li>
-					<li class="right"><a class="login" href="../login.html">Logga in (endast personal)</a></li>
-				</ul>
-			</nav><!-- End Nav -->
-
+﻿<?php include("../incl/header.php");
+?>
 			<div id="content">
 				<section id="mainColumn">
 					<h2>Din anmälan av miljöbrott</h2>
@@ -49,10 +25,5 @@
 					<img src="../images/pic_8.jpg" height="162" alt="Närbild av en fotboll" />
 				</aside><!-- End Right -->
 	  
-				<footer>
-					&copy; Småstads Kommun All rights reserved.
-				</footer>
-			</div><!-- End Content -->
-		</div><!-- End Wrapper -->
-	</body>
-</html>
+<?php include("../incl/footer.php");
+?>
