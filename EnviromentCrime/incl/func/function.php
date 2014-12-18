e6836a8d750cf222b1dc72d03770daa360371c4a
biramaster <biramaster001@gmail.com>
2014-12-18 13:37:17 +0100

    Create database

---
+++ EnviromentCrime/incl/func/function.php
@@ -0,0 +1,14 @@
+﻿<?php
+//
+// Connect to the database
+//
+
+function ConnectDBA()
+{
+	$db = new PDO("sqlite:blocket.sqlite");
+
+	// Display errors, but continue
+	$db->setAttribute(PDO::ATTR_ERRMODE,
+					  PDO::ERRMODE_WARNING);
+}				  
+?>
